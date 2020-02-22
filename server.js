112d0ddd28420887c1abb8f576e360283f17c456
mmgusta <gustavo.marinelli@gmail.com>
2020-02-22 15:01:53 -0300

    deploy heroku

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/cadestag'));
+app.use(express.static(__dirname + '/dist/cad-estag'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/cadestag/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/cad-estag/index.html'));
 });
 
 // default Heroku PORT
